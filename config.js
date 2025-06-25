a35595e97bd55b2517219ac8d63340dfba07a42b
zeroCountersteer <fireboll.tv@gmail.com>
2025-06-25 10:31:43 +0500

    upd

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ const CONFIG = {
   AUTH0_CLIENT_ID: 'cI6byoFfMLjtDyBpbAr0pZXjB35pqb6L',
   GITHUB_REPO: 'zeroCountersteer/income-and-chores-tracker',
   GITHUB_FILE_PATH: 'data.json',
-  GITHUB_BRANCH: 'main',
-  GITHUB_PAT: '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'
+  GITHUB_BRANCH: 'main'
 };
--- github.js
+++ github.js
@@ -23,19 +23,21 @@ async function getFile() {
 
 async function saveFile(data) {
   const content = btoa(unescape(encodeURIComponent(JSON.stringify(data, null, 2))));
-  const res = await fetch(`https://api.github.com/repos/${CONFIG.GITHUB_REPO}/contents/${CONFIG.GITHUB_FILE_PATH}`, {
+  const res = await fetch(`https://api.github.com/repos/${CONFIG.GITHUB_REPO}/contents/tmp-data.json`, {
     method: 'PUT',
     headers: {
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      message: 'Update data.json',
+      message: 'Temp save from frontend',
       content,
       sha: fileSha,
       branch: CONFIG.GITHUB_BRANCH
     })
   });
+
   const json = await res.json();
+  if (!res.ok) throw new Error(JSON.stringify(json));
   fileSha = json.content.sha;
 }
